ba479e9661d06d26e6c1b58b2eb3c4bf0ef4c993
@laura.lululand <laura.lululand@gmail.com>
2020-08-18 16:57:06 +0100

    initial build

--- js/adapt-walkgrove-dialogue.js
+++ js/adapt-walkgrove-dialogue.js
@@ -1,59 +1,12 @@
 define([
   'core/js/adapt',
-  'core/js/views/componentView',
-  'core/js/models/componentModel'
-], function(Adapt, ComponentView, ComponentModel) {
-
-  var DialogueView = ComponentView.extend({
-
-    preRender: function() {
-      this.checkIfResetOnRevisit();
-    },
-
-    postRender: function() {
-      this.setReadyStatus();
-
-      this.setupInview();
-    },
-
-    setupInview: function() {
-      var selector = this.getInviewElementSelector();
-      if (!selector) {
-        this.setCompletionStatus();
-        return;
-      }
-
-      this.setupInviewCompletion(selector);
-    },
-
-    /**
-     * determines which element should be used for inview logic - body, instruction or title - and returns the selector for that element
-     */
-    getInviewElementSelector: function() {
-      if (this.model.get('body')) return '.component__body';
-
-      if (this.model.get('instruction')) return '.component__instruction';
-
-      if (this.model.get('displayTitle')) return '.component__title';
-
-      return null;
-    },
-
-    checkIfResetOnRevisit: function() {
-      var isResetOnRevisit = this.model.get('_isResetOnRevisit');
-
-      // If reset is enabled set defaults
-      if (isResetOnRevisit) {
-        this.model.reset(isResetOnRevisit);
-      }
-    }
-  },
-  {
-    template: 'dialogue'
-  });
+  './dialogueView',
+  'core/js/models/itemsComponentModel'
+], function(Adapt, DialogueView, ItemsComponentModel) {
 
   return Adapt.register('dialogue', {
-    model: ComponentModel.extend({}),// create a new class in the inheritance chain so it can be extended per component type if necessary later
+    model: ItemsComponentModel,
     view: DialogueView
   });
+
 });
---
+++ js/dialogueView.js
@@ -0,0 +1,92 @@
+define([
+  'core/js/adapt',
+  'core/js/views/componentView'
+], function(Adapt, ComponentView, MODE) {
+  'use strict';
+
+  var DialogueView = ComponentView.extend({
+
+    _isInitial: true,
+
+    events: {
+      'click .js-dialogue-start-click': 'onStartClicked'
+    },
+
+    preRender: function() {
+      var objCharacter = {},
+      arrCharacters = [],
+      inArray = function( mxdNeedle, arrHaystack ) {
+          for( var mxdValue in arrHaystack ) {
+              if( arrHaystack[mxdValue] === mxdNeedle ) {
+                  return true;
+              }
+          }
+
+          return false;
+      };
+
+      for( var intCharacter in this.model.get( '_characters' ) ) {
+          objCharacter[this.model.get( '_characters' )[intCharacter]] = this.model.get( '_characters' )[intCharacter];
+          arrCharacters.push(objCharacter[this.model.get( '_characters' )[intCharacter]]);
+      }
+
+      Handlebars.registerHelper( 'characterName',
+          function( strCharacter ) {
+              return arrCharacters[strCharacter].name || '';
+          }
+      );
+
+      Handlebars.registerHelper( 'characterImage',
+          function( strCharacter ) {
+            return arrCharacters[strCharacter]._image || '';
+          }
+      );
+
+      this.checkIfResetOnRevisit();
+    },
+
+    postRender: function() {
+      this.setReadyStatus();
+      this.setupInview();
+    },
+
+    onStartClicked: function() {
+      this.$('.dialogue__main').addClass('is-visible');
+      this.$('.dialogue__button').removeClass('is-visible');
+
+      this.setupInview();
+    },
+
+    setupInview: function() {
+      var selector = this.getInviewElementSelector();
+      if (!selector) {
+        this.setCompletionStatus();
+        return;
+      }
+      this.setupInviewCompletion(selector);
+    },
+
+    /**
+     * determines which element should be used for inview logic - body, instruction or title - and returns the selector for that element
+     */
+    getInviewElementSelector: function() {
+      return '.dialogue__complete';
+    },
+
+
+    checkIfResetOnRevisit: function() {
+      var isResetOnRevisit = this.model.get('_isResetOnRevisit');
+
+      // If reset is enabled set defaults
+      if (isResetOnRevisit) {
+        this.model.reset(isResetOnRevisit);
+      }
+    }
+
+    
+
+  });
+
+  return DialogueView;
+
+});
